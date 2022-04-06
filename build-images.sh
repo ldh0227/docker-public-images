844aef2209a67ba83f222b45783ce11900223224
Dong-ha, Lee <ldh0227@gmail.com>
2022-04-06 13:24:33 +0000

    change dockerhub repo to ldh0227

--- build-images.sh
+++ build-images.sh
@@ -11,9 +11,9 @@ DOCKER_TAG="${DOCKER_TAG:-master}"
 
 # Build images.
 for image in $IMAGES; do
-	echo "Building jbonjean/$image"
-	docker build "$image" --tag "jbonjean/$image:$DOCKER_TAG" --build-arg DOCKER_TAG="$DOCKER_TAG"
-	docker tag "jbonjean/$image:$DOCKER_TAG" "jbonjean/$image:latest"
+	echo "Building ldh0227/$image"
+	docker build "$image" --tag "ldh0227/$image:$DOCKER_TAG" --build-arg DOCKER_TAG="$DOCKER_TAG"
+	docker tag "ldh0227/$image:$DOCKER_TAG" "ldh0227/$image:latest"
 done
 
 echo "Build success"
@@ -25,14 +25,14 @@ fi
 
 # Push images, with the new tag.
 for image in $IMAGES; do
-	echo "Pushing jbonjean/$image:$DOCKER_TAG"
-	docker push "jbonjean/$image:$DOCKER_TAG"
+	echo "Pushing ldh0227/$image:$DOCKER_TAG"
+	docker push "ldh0227/$image:$DOCKER_TAG"
 done
 
 # Update latest tag.
 for image in $IMAGES; do
-	echo "Pushing jbonjean/$image:latest"
-	docker push "jbonjean/$image:latest"
+	echo "Pushing ldh0227/$image:latest"
+	docker push "ldh0227/$image:latest"
 done
 
 echo "Done"
